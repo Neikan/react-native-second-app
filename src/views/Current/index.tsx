fee42d392168890b9786449b1a8f45d86078eb3c
Neikan <sladkov.e.m@outlook.com>
2021-11-28 12:19:01 +0300

    feat(Current): add removing post

--- src/views/Current/index.tsx
+++ src/views/Current/index.tsx
@@ -1,31 +1,64 @@
 import React, { FC, useLayoutEffect } from 'react'
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Alert, Button, Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 
+import { DATA } from '@/consts/data'
 import { Color } from '@/consts/themes'
 
 export const Current: FC<any> = ({ navigation, route }) => {
-  const goToMain = (): void => navigation.navigate('Main')
+  const { params } = route
+
+  const post = DATA.find((item) => item.id === params.postId)
+
+  const handleRemovePost = (): void => {
+    Alert.alert(
+      'Удаление поста',
+      'Вы уверены, что хотите удалить пост?',
+      [
+        {
+          text: 'Подтвердить',
+          onPress: () => {}
+        },
+        {
+          text: 'Отмена',
+          style: 'cancel'
+        }
+      ],
+      { cancelable: true }
+    )
+  }
 
   useLayoutEffect(() => {
-    const { params } = route
-
     const date = new Date(params.postDate).toLocaleDateString()
 
     navigation.setOptions({ headerTitle: 'Пост ' + String(params.postId) + ' от ' + date })
   }, [navigation, route])
 
   return (
-    <View style={styles.view}>
-      <Text>{route.params.postId}</Text>
-      <Button title='Go to Main' onPress={goToMain} color={Color.PRIMARY_600} />
-    </View>
+    <ScrollView style={styles.view}>
+      <View>
+        <Image style={styles.image} source={{ uri: post?.img }} />
+        <View style={styles.textView}>
+          <Text style={styles.text}>{post?.text}</Text>
+        </View>
+      </View>
+      <Button title='Удалить' onPress={handleRemovePost} color={Color.DANGER_500} />
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
   view: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    margin: 16
+  },
+  textView: {
+    alignItems: 'center'
+  },
+  text: {
+    padding: 16
+  },
+  image: {
+    height: 200,
+    width: '100%'
   }
 })
