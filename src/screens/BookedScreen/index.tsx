19372db9e7c0f70a9997a111fdbbedb84df4ba6c
Neikan <sladkov.e.m@outlook.com>
2021-11-28 16:35:37 +0300

    feat(BookedScreen): refactor component

--- src/screens/BookedScreen/index.tsx
+++ src/screens/BookedScreen/index.tsx
@@ -1,20 +1,12 @@
 import React, { FC, useLayoutEffect } from 'react'
-import { FlatList, StyleSheet, View } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 
 import { DATA } from '@/consts/data'
 
-import { Post } from '@/components/elements/Post'
 import { AppHeaderIcon } from '@/components/ui/AppHeaderIcon'
 
 import { IPost } from '@/types'
-
-const styles = StyleSheet.create({
-  view: {
-    flex: 1,
-    padding: 16
-  }
-})
+import { AppPostsFlatList } from '@/components/ui/AppPostsFlatList'
 
 export const BookedScreen: FC<any> = ({ navigation }) => {
   const goToCurrent = (post: IPost): void =>
@@ -22,24 +14,15 @@ export const BookedScreen: FC<any> = ({ navigation }) => {
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      headerRight: () => (
+      headerLeft: () => (
         <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
-          <Item title='photo' iconName='ios-camera' onPress={() => null} />
+          <Item title='drawer' iconName='ios-menu' onPress={() => null} />
         </HeaderButtons>
       )
     })
   }, [navigation])
 
-  const bookedData = DATA.filter((post) => post.booked)
-
   return (
-    <View style={styles.view}>
-      <FlatList
-        data={bookedData}
-        keyExtractor={(post) => post.id.toString()}
-        renderItem={({ item }) => <Post post={item} onOpen={goToCurrent} />}
-        showsVerticalScrollIndicator={false}
-      />
-    </View>
+    <AppPostsFlatList data={DATA.filter((post) => post.booked)} onOpen={goToCurrent} />
   )
 }
