4cbb3806b8f5bbbf61be18e67477d5492a0d8417
Neikan <sladkov.e.m@outlook.com>
2021-11-28 16:36:09 +0300

    feat(AppNavigation): refactor component

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -9,20 +9,35 @@ import { Color } from '@/consts/themes'
 import { BookedStackScreen } from '@/navigation/BookedStackScreen'
 import { MainStackScreen } from '@/navigation/MainStackScreen'
 
+// Необходимо разобраться по какой причине использование createMaterialBottomTabNavigator приводит к ошибкам
+// const Tab = Platform.OS === 'android' ? createMaterialBottomTabNavigator() : createBottomTabNavigator()
 const Tab = createBottomTabNavigator()
 
 export const AppNavigation: FC = () => (
   <NavigationContainer>
     <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ color }) => <Ionicons name={navIcons[route.name]} size={24} color={color} />,
+      screenOptions={{
         tabBarActiveTintColor: Color.PRIMARY_600,
         tabBarInactiveTintColor: Color.GRAY_500,
         headerShown: false
-      })}
+      }}
     >
-      <Tab.Screen name='MainStackScreen' component={MainStackScreen} options={{ title: 'Мой блог' }} />
-      <Tab.Screen name='BookedStackScreen' component={BookedStackScreen} options={{ title: 'Избранное' }} />
+      <Tab.Screen
+        name='MainStackScreen'
+        component={MainStackScreen}
+        options={{
+          title: 'Все',
+          tabBarIcon: ({ color }) => <Ionicons name={navIcons.MainStackScreen} size={24} color={color} />
+        }}
+      />
+      <Tab.Screen
+        name='BookedStackScreen'
+        component={BookedStackScreen}
+        options={{
+          tabBarLabel: 'Избранное',
+          tabBarIcon: ({ color }) => <Ionicons name={navIcons.BookedStackScreen} size={24} color={color} />
+        }}
+      />
     </Tab.Navigator>
   </NavigationContainer>
 )
