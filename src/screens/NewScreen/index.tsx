bf786b80c57ceee846215e4ae74058b4e86a5d15
Neikan <sladkov.e.m@outlook.com>
2021-11-28 17:53:00 +0300

    feat(NewScreen): refactor component

--- src/screens/NewScreen/index.tsx
+++ src/screens/NewScreen/index.tsx
@@ -1,5 +1,8 @@
-import React, { FC } from 'react'
+import React, { FC, useLayoutEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+
+import { AppHeaderIcon } from '@/components/ui/AppHeaderIcon'
 
 const styles = StyleSheet.create({
   view: {
@@ -9,7 +12,17 @@ const styles = StyleSheet.create({
   }
 })
 
-export const NewScreen: FC = () => {
+export const NewScreen: FC<any> = ({ navigation }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+          <Item title='drawer' iconName='ios-menu' onPress={navigation.toggleDrawer} />
+        </HeaderButtons>
+      )
+    })
+  }, [navigation])
+
   return (
     <View style={styles.view}>
       <Text>New</Text>
