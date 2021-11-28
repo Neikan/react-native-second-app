a871f74293641eed0726ab964a7019e41b3cdd71
Neikan <sladkov.e.m@outlook.com>
2021-11-28 18:28:58 +0300

    feat(AppDrawer): add styles

--- src/navigation/AppDrawer/index.tsx
+++ src/navigation/AppDrawer/index.tsx
@@ -4,13 +4,17 @@ import { createDrawerNavigator } from '@react-navigation/drawer'
 import { AppTab } from '@/navigation/AppTab'
 import { NewStackScreen } from '@/navigation/NewStackScreen'
 import { AboutStackScreen } from '@/navigation/AboutStackScreen'
+import { Color } from '@/consts/themes'
 
 const Drawer = createDrawerNavigator()
 
 export const AppDrawer: FC = () => (
   <Drawer.Navigator
     screenOptions={{
-      headerShown: false
+      headerShown: false,
+      drawerActiveTintColor: Color.PRIMARY_600,
+      drawerActiveBackgroundColor: Color.PRIMARY_200,
+      drawerInactiveTintColor: Color.GRAY_600
     }}
   >
     <Drawer.Screen name='AppTab' component={AppTab} options={{ drawerLabel: 'Главная' }} />
