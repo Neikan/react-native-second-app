36f4b7333f9ecace91c1f92360b8be98c57b22ef
Neikan <sladkov.e.m@outlook.com>
2021-11-28 17:42:00 +0300

    feat(AppTab): add component

---
+++ src/navigation/AppTab/index.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons } from '@expo/vector-icons'
+
+import { navIcons } from '@/consts/navItems'
+import { Color } from '@/consts/themes'
+
+import { BookedStackScreen } from '@/navigation/BookedStackScreen'
+import { MainStackScreen } from '@/navigation/MainStackScreen'
+
+// Необходимо разобраться по какой причине использование createMaterialBottomTabNavigator приводит к ошибкам
+// const Tab = Platform.OS === 'android' ? createMaterialBottomTabNavigator() : createBottomTabNavigator()
+const Tab = createBottomTabNavigator()
+
+export const AppTab: FC = () => (
+  <Tab.Navigator
+    screenOptions={{
+      tabBarActiveTintColor: Color.PRIMARY_600,
+      tabBarInactiveTintColor: Color.GRAY_500,
+      headerShown: false
+    }}
+  >
+    <Tab.Screen
+      name='MainStackScreen'
+      component={MainStackScreen}
+      options={{
+        title: 'Все',
+        tabBarIcon: ({ color }) => <Ionicons name={navIcons.MainStackScreen} size={24} color={color} />
+      }}
+    />
+    <Tab.Screen
+      name='BookedStackScreen'
+      component={BookedStackScreen}
+      options={{
+        tabBarLabel: 'Избранное',
+        tabBarIcon: ({ color }) => <Ionicons name={navIcons.BookedStackScreen} size={24} color={color} />
+      }}
+    />
+  </Tab.Navigator>
+)
