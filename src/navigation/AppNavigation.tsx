5120cd7611eae1edb2d47d7120862d23d31ab7da
Neikan <sladkov.e.m@outlook.com>
2021-11-28 15:24:17 +0300

    feat(AppNavigation): refactor component

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -1,52 +1,28 @@
 import React, { FC } from 'react'
-import { Text, Platform } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator, NativeStackNavigationOptions } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons } from '@expo/vector-icons'
 
-import { Main as MainView } from '@/views/Main'
-import { Current as CurrentView } from '@/views/Current'
+import { navIcons } from '@/consts/navItems'
 import { Color } from '@/consts/themes'
 
-const Stack = createNativeStackNavigator()
+import { BookedStackScreen } from '@/navigation/BookedStackScreen'
+import { MainStackScreen } from '@/navigation/MainStackScreen'
 
-const styles: NativeStackNavigationOptions =
-  Platform.OS === 'ios'
-    ? {
-        headerStyle: {
-          backgroundColor: Color.LIGHT
-        },
-        headerTintColor: Color.PRIMARY_600
-      }
-    : {
-        headerStyle: {
-          backgroundColor: Color.PRIMARY_600
-        },
-        headerTintColor: Color.LIGHT
-      }
+const Tab = createBottomTabNavigator()
 
-export const AppNavigation: FC = () => {
-  return (
-    <NavigationContainer fallback={<Text>Loading...</Text>}>
-      <Stack.Navigator>
-        <Stack.Screen
-          name='Main'
-          component={MainView}
-          options={{
-            title: 'Мой блог',
-            animation: 'slide_from_left',
-            ...styles
-          }}
-        />
-        <Stack.Screen
-          name='Current'
-          component={CurrentView}
-          options={{
-            title: 'Пост',
-            animation: 'slide_from_right',
-            ...styles
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
-}
+export const AppNavigation: FC = () => (
+  <NavigationContainer>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color }) => <Ionicons name={navIcons[route.name]} size={24} color={color} />,
+        tabBarActiveTintColor: Color.PRIMARY_600,
+        tabBarInactiveTintColor: Color.GRAY_500,
+        headerShown: false
+      })}
+    >
+      <Tab.Screen name='MainStackScreen' component={MainStackScreen} options={{ title: 'Мой блог' }} />
+      <Tab.Screen name='BookedStackScreen' component={BookedStackScreen} options={{ title: 'Избранное' }} />
+    </Tab.Navigator>
+  </NavigationContainer>
+)
