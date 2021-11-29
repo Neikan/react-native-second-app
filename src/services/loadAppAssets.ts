0fb8dc895c5c8e40911c4fc0c9c8c9a4440530a6
Neikan <sladkov.e.m@outlook.com>
2021-11-29 17:24:47 +0300

    feat(services): add database to project

--- src/services/loadAppAssets.ts
+++ src/services/loadAppAssets.ts
@@ -1,1 +1,11 @@
-export const loadAppAssets = async (): Promise<void> => {}
+import { DB } from '@/services/db'
+import { Alert } from 'react-native'
+
+export const loadAppAssets = async (): Promise<void> => {
+  try {
+    await DB.init()
+  } catch (error) {
+    Alert.alert('Ошибка инициализации приложения')
+  }
+
+}
